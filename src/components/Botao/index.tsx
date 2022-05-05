5345f5ece49deeb29547300de4339f6ff30641b8
Andressa Reis Araujo <andressareis98@outlook.com>
2022-05-05 17:22:12 -0300

    feat: countdown

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,13 +2,14 @@ import style from './Botao.module.scss';
 
 interface Props{
     type?: 'button' | 'submit' | 'reset' | undefined
-    texto: string
+    texto: string,
+    onClick?: () => void
 }
 
-export function Botao({type = 'button', texto} : Props){
+export function Botao({type = 'button', texto, onClick} : Props){
     return(
         <div >
-            <button type={type} className={style.botao}>{texto}</button>
+            <button onClick={onClick} type={type} className={style.botao}>{texto}</button>
         </div>
     )
 }
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -12,6 +12,15 @@ interface Props{
 export function Cronometro({selecionado}: Props){
     const [tempo, setTempo] = useState<number>();
 
+    const regressiva = (contador: number = 0) => {
+        setTimeout(() => {
+            if(contador > 0){
+                setTempo(contador -1);
+                return regressiva (contador - 1)
+            }
+        }, 1000)
+    }
+
     useEffect(() => {
         if(selecionado?.tempo){
             setTempo(tempoParaSegundos(selecionado.tempo))
@@ -24,7 +33,7 @@ export function Cronometro({selecionado}: Props){
             <div className={style.relogioWrapper}>
                 <Relogio tempo = {tempo}/>
             </div>
-            <Botao texto="Começar"/>
+            <Botao onClick={()=> regressiva(tempo)} texto="Começar"/>
         </div>
     )
 }
